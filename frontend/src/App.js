1bca86892c879ab494dc3b0c0d918921eda041ae
daniel gaona <daniel.gaona@pjedomex.gob.mx>
2023-06-03 13:06:26 -0600

    se agrega respuesta a app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import axiosClient from "./config/AxiosClient";
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-
+  const [post, setPost] = useState({});
   const posts = () => {
-    axiosClient.get('/posts/1')
+    axiosClient.get('/posts?_start=0&_end=30')
       .then(res => {
-        console.log(res);
+        console.log(res.data);
+        setPost(res.data);
       });
   };
   
@@ -19,7 +20,13 @@ function App() {
   return (
     <>
     <h1>Prueba</h1>
-
+    <ul className="list-group list-group-numbered">
+        {Object.keys(post).map((p) => (
+          <li className="list-group-item" key={p} id={p} onClick={(e) => {
+            console.log(p);
+          }}>{post[p].title}</li>
+        ))}
+      </ul>
     </>
   );
 }
