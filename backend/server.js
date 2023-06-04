c8be76902452b4c8a64a389b72e74ce1f972e6cc
daniel gaona <daniel.gaona@pjedomex.gob.mx>
2023-06-03 21:41:07 -0600

    se agrega conexion a firbase

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-// server.js
-const jsonServer = require('json-server')
+const  { initializeApp } = require("firebase/app");
+const { getDatabase,ref,get,child } = require("firebase/database");
+const jsonServer = require('json-server');
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -7,8 +8,29 @@ const middlewares = jsonServer.defaults()
 require("dotenv").config();
 const port = `${process.env.REACT_APP_PORT}`;
 
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+}
+
+const app = initializeApp(firebaseConfig);
+
+function posts(){
+  const dbRef = ref(getDatabase());
+  const resultado =get(child(dbRef, "posts" )).then((posts)=>{
+    return posts.val();
+  })
+  console.log("Resultado",resultado);
+
+}
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
+  posts();
   console.log('JSON Server is running',process.env.REACT_APP_PORT)
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,37 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   const [post, setPost] = useState({});
+  const [paginas, setPaginas] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recordsPerPage] = useState(2);
+
   const posts = () => {
     axiosClient.get('/posts?_start=0&_end=30')
       .then(res => {
-        console.log(res.data);
+        console.log(res.data.length);
+        setPaginas((Math.ceil(res.data.length / recordsPerPage))-1);
         setPost(res.data);
       });
   };
+
+  const siguientePagina = () => {
+    if (currentPage <= paginas) {
+      setCurrentPage(currentPage + 1)
+      console.log(currentPage);
+    }else{
+      console.log("llegaste a la ultima pagina");
+    }
+  }
+  const anteriorPagina = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+      console.log(currentPage);
+    }else{
+      console.log("llegaste a la primer pagina");
+    }
+  }
+
+  const pageNumbers = [...Array(paginas + 1).keys()].slice(1)
   
   useEffect(() => {
     posts();
@@ -19,7 +43,39 @@ function App() {
 
   return (
     <>
-    <h1>Prueba</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Practica</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Posts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Coments</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Profiles</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+<h1>Posts</h1>
+<nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item"><a className="page-link" href="#" onClick={anteriorPagina}>Anterior</a></li>
+          {pageNumbers.map((pagina) => (
+            <li key={pagina} className="page-item"><a className="page-link" href="#" id={pagina} onClick={(event) => {
+              setCurrentPage(event.currentTarget.id)
+            }}>{pagina}</a></li>
+          ))}
+          <li className="page-item"><a className="page-link" href="#" onClick={siguientePagina}>Siguiente</a></li>
+        </ul>
+      </nav>
     <ul className="list-group list-group-numbered">
         {Object.keys(post).map((p) => (
           <li className="list-group-item" key={p} id={p} onClick={(e) => {
