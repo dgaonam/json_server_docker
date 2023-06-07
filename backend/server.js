6513424b5375c1b6713128f935f9fd3c1bb815db
daniel gaona <daniel.gaona@pjedomex.gob.mx>
2023-06-06 22:17:29 -0600

    se agrega conexion a base de datos de firbase y generacion de json al vuelo

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const fs = require('fs');
 
 require("dotenv").config();
 const port = `${process.env.REACT_APP_PORT}`;
@@ -20,17 +21,50 @@ var firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-function posts(){
+
+
+async function db(){
+  let json = "{";
   const dbRef = ref(getDatabase());
-  const resultado =get(child(dbRef, "posts" )).then((posts)=>{
+  const posts =await get(child(dbRef, "posts" )).then((posts)=>{
     return posts.val();
   })
-  console.log("Resultado",resultado);
+  console.log("Resultado",posts);
 
+  json +="\"posts\":["
+  posts.forEach(post => {
+    json +=JSON.stringify(post)+","
+  });
+  json=json.substring(0, json.length-1)
+  json+="],\"comments\":["
+
+  const comments =await get(child(dbRef, "comments" )).then((comments)=>{
+    return comments.val();
+  })
+  console.log("Resultado",comments);
+  comments.forEach(comment => {
+    json +=JSON.stringify(comment)+","
+  });
+  json=json.substring(0, json.length-1)
+  json+="],\"profile\":["
+  const profiles =await get(child(dbRef, "profile" )).then((profiles)=>{
+    return profiles.val();
+  })
+  console.log("Resultado",profiles);
+  profiles.forEach(profile => {
+    json +=JSON.stringify(profile)+","
+  });
+  json=json.substring(0, json.length-1)
+  json+="]}"
+  console.log(json);
+  const filename = 'db.json';
+  fs.writeFileSync(filename, json)
 }
 server.use(middlewares)
 server.use(router)
 server.listen(port, () => {
-  posts();
+  db();
+  
   console.log('JSON Server is running',process.env.REACT_APP_PORT)
+
 })
